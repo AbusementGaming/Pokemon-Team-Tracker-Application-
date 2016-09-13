a46563eff90c6d3e3bbd63a8f86df897e22fe409
Chris <Chris@CHRISTOSHIBA>
2016-09-13 19:53:11 -0400

    Added .ico

--- PokeTracker/Twitch-Pokemon-Tracker/MainForm.Designer.vb
+++ PokeTracker/Twitch-Pokemon-Tracker/MainForm.Designer.vb
@@ -22,6 +22,7 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
         Me.SuspendLayout()
         '
         'MainForm
@@ -29,6 +30,7 @@ Partial Class MainForm
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(384, 361)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "MainForm"
         Me.Text = "PokeTracker"
         Me.ResumeLayout(False)
